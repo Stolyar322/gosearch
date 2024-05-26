fd91228ee15c0ff7b82d9fb1e0fd62c97f26b233
Aleksandr <stolyar3222@gmail.com>
2024-05-26 22:22:31 +0300

    Added writing and reading links at file

--- crawler/cmd/main.go
+++ crawler/cmd/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"gosearch/crawler/pkg/crawler"
 	"gosearch/crawler/pkg/crawler/spider"
-	"gosearch/crawler/pkg/index"
-	"math/rand"
-	"sort"
-	"time"
+	"io"
+	"os"
+	"strings"
 )
 
 const depth int = 2
@@ -20,12 +21,82 @@ func flagInit() {
 	flag.Parse()
 }
 
+const filePath = "./../links.txt"
+
 func main() {
 	flagInit()
 	if needle == "" {
 		fmt.Println("Needle flag is empty")
 		return
 	}
+
+	_, err := os.Stat(filePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fileStorageInit()
+		} else {
+			panic(err)
+		}
+	}
+
+	links, err := search(filePath, needle)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(links) == 0 {
+		fmt.Printf("Links with %s not found", needle)
+		return
+	}
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
+}
+
+func search(pathFile string, needle string) (res []string, err error) {
+	file, err := os.Open(pathFile)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+
+	}(file)
+
+	var links []string
+	scanner := bufio.NewReader(file)
+	for {
+		buff, _, err := scanner.ReadLine()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return []string{}, err
+		}
+
+		link := fmt.Sprintf("%s", buff)
+		if strings.Contains(link, needle) {
+			links = append(links, link)
+		}
+	}
+
+	return links, nil
+}
+
+func fileStorageInit() {
+	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
 
 	scanner := spider.New()
 	resources := [...]string{"https://golang-org.appspot.com/", "https://go.dev/"}
@@ -40,31 +111,10 @@ func main() {
 		scanResult = append(scanResult, document...)
 	}
 
-	source := rand.NewSource(time.Now().UnixNano())
-	randomizer := rand.New(source)
-	for i := range scanResult {
-		scanResult[i].ID = randomizer.Int()
-	}
-
-	sort.Slice(scanResult, func(i, j int) bool {
-		return scanResult[i].ID < scanResult[j].ID
-	})
-
-	indexBuilder := index.NewReverseIndex()
-	indexes := indexBuilder.CreateIndex(&scanResult)
-
-	linkIds, ok := indexes[needle]
-	if !ok {
-		fmt.Printf("Link with work %s not found", needle)
-		return
-	}
-
-	for _, id := range linkIds {
-		link := sort.Search(len(scanResult), func(i int) bool {
-			return scanResult[i].ID >= id
-		})
-		if link < len(scanResult) && scanResult[link].ID == id {
-			fmt.Println(scanResult[link].URL)
+	for _, result := range scanResult {
+		_, err := file.Write([]byte(result.URL + "\n"))
+		if err != nil {
+			panic(err)
 		}
 	}
 }
